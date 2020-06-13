df93a3ebf8a23f59631d14cfe51efc4760f8ac33
Dave Hng <dave.hng@gmail.com>
2020-06-14 07:34:07 +0800

    Update PID tuning parameters for 12V MK2.5S

--- mk2.5s-btt-002-hemera/Configuration.h
+++ mk2.5s-btt-002-hemera/Configuration.h
@@ -481,17 +481,19 @@
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 
 #if ENABLED(PIDTEMP)
-  //#define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
-  //#define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
-  //#define PID_PARAMS_PER_HOTEND // Uses separate PID parameters for each extruder (useful for mismatched extruders)
+  #define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
+  #define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
+  #define PID_PARAMS_PER_HOTEND // Uses separate PID parameters for each extruder (useful for mismatched extruders)
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
-  // BTT002 MK3S // revise
-  #define DEFAULT_Kp 16.13
-  #define DEFAULT_Ki 1.1625
-  #define DEFAULT_Kd 56.23
+  // 12V MK2.5S with BTT002
+  // Kp: 27.68 Ki: 1.79 Kd: 106.86
+  // M301 P27.68 I1.79 D106.86
+  #define DEFAULT_Kp 27.68
+  #define DEFAULT_Ki 1.79
+  #define DEFAULT_Kd 106.86  
 
 #endif // PIDTEMP
 
@@ -528,10 +530,12 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  // BTT002 MK3S // need to revise
-  #define  DEFAULT_bedKp 126.13
-  #define  DEFAULT_bedKi 4.30
-  #define  DEFAULT_bedKd 924.76
+  // 12V MK2.5S with BTT002
+  // Kp: 64.56 Ki: 4.29 Kd: 647.33
+  // M304 P64.56 I4.29 D647.33
+  #define DEFAULT_bedKp 64.56
+  #define DEFAULT_bedKi 4.29
+  #define DEFAULT_bedKd 647.33
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
